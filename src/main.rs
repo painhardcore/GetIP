aeafec71e959698e60856561a7f5674515223fde
Andrey Yurchenkov <painhardcore@gmail.com>
2019-04-21 04:16:08 +0300

    cli wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 extern crate clap;
-use clap::App;
+use clap::{Arg, App};
 
 fn main() {
-  App::new("getip").version("v0.0.1-beta").get_matches();
+  let matches = App::new("getip").version("v0.0.1-beta").author("Andrey Y. <painhardcore@gmail.com>")
+                               .arg(Arg::with_name("cluster")
+                               .help("cluster")
+                               .required(true)
+                               .index(1))
+                               .get_matches();
+
+  let input = matches.value_of("cluster").unwrap().to_string();
+  match input.as_ref() {
+    "list" => get_clusters_list(),
+    _ => get_cluster_info(&input),
+  }   
+}
+
+fn get_cluster_info(name: &String){
+    println!("Cluster name : {}",name)
+}
+fn get_clusters_list(){
+    println!("Cluster list")
 }
